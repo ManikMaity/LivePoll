60d083a86dbe4359bb49159b9b9f759b325f9305
ManikMaity <manikmaity156@gmail.com>
2026-03-28 22:21:46 +0530

    chore: Added github action folder

--- backend/src/controllers/user.controller.js
+++ backend/src/controllers/user.controller.js
@@ -29,18 +29,20 @@ export async function signinController(req, res) {
   try {
     const { email, password } = req.body;
     const { token, userData } = await signinService(email, password);
-    res.cookie("livepoll-access-token", token, {
-      httpOnly: true,    
-      secure: true,      
-      sameSite: "None", 
-      expires: new Date(Date.now() + 24 * 60 * 60 * 1000 * 365), // 1 year
-      path: "/"
-    }).status(200).json({
-        success : true,
-        message : "User signedin successfully.",
-        user : userData
-    });
-
+    res
+      .cookie("livepoll-access-token", token, {
+        httpOnly: true,
+        secure: true,
+        sameSite: "None",
+        expires: new Date(Date.now() + 24 * 60 * 60 * 1000 * 365), // 1 year
+        path: "/",
+      })
+      .status(200)
+      .json({
+        success: true,
+        message: "User signedin successfully.",
+        user: userData,
+      });
   } catch (err) {
     console.log(err);
     if (err.statusCode) {
@@ -58,33 +60,30 @@ export async function signinController(req, res) {
 }
 
 export const checkUser = async (req, res) => {
-  try{
-      res.json({
-          success : true,
-          message : "Found",
-          data : req.user
-      })
+  try {
+    res.json({
+      success: true,
+      message: "Found",
+      data: req.user,
+    });
+  } catch (err) {
+    res.status(500).json({
+      success: false,
+      message: err.message,
+    });
   }
-  catch(err){
-      res.status(500).json({
-          success : false,
-          message : err.message
-      })
-  }
-}
+};
 
 export async function logOutController(req, res) {
-  try{
+  try {
     res.clearCookie("livepoll-access-token").status(200).json({
-      success : true,
-      message : "User logged out successfully."
-    })
-  }
-  catch(err){
+      success: true,
+      message: "User logged out successfully.",
+    });
+  } catch (err) {
     res.status(500).json({
-      success : false,
-      message : err.message
-    })
+      success: false,
+      message: err.message,
+    });
   }
 }
-
